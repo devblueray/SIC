970842efb993e5c14c47dade7537a762af580e20
jlane <devblueray@gmail.com>
2017-05-19 13:31:10 -0700

    Working struct and data going into mongo

--- main.go
+++ main.go
@@ -2,74 +2,78 @@ package main
 
 import (
   "fmt"
-  "log"
+ // "log"
   "gopkg.in/mgo.v2"
-  "gopkg.in/mgo.v2/bson"
+ // "gopkg.in/mgo.v2/bson"
   "encoding/json"
+
+
+  "log"
 )
 
 type Employee struct {
-  Name string `bson:"name"`
-  Phone string `bson:"phone"`
-  Email string `bson:"email"`
-  Department string `bson:"department"`
-  Manager string `bson:"manager"`
-  EmailGroups []string `bson:"emailgroups"`
-  Assets map[string]Assets `bson:",inline"`  
+  Name string `json:"name"`
+  Phone string `json:"phone"`
+  Email string `json:"email"`
+  Department string `json:"department"`
+  Manager string `json:"manager"`
+  EmailGroups []string `json:"email_groups"`
+  Assets map[string]Asset `json:"assetlist"`
 }
 
-type Assets struct {
-  AssetType string `bson:"assettype"`
-  AssetProperties map[string]string `bson:"assetproperties"`
+type Asset struct {
+  Tag string `json:"asset_tag"`
+  Encrypted int `json:"encrypted"`
 }
 
-type AssetProperties struct {
-  AssetTag string `bson:"serialnumber"`
-  Encrypted int `bson:"encrypted"`
-}
-   
 func main() {
-  session,err := mgo.Dial("127.0.0.1")
-  if err != nil {
-    fmt.Println("Mongo Dialer")
-    panic(err)
-  }
+  session, _ := mgo.Dial("127.0.0.1")
+  defer session.Close()
 
-session.SetMode(mgo.Monotonic,true)
+  session.SetMode(mgo.Monotonic, true)
+  c:= session.DB("sic").C("employees")
 
-c := session.DB("sic").C("employees")
 
-df := `
+  df := `
 {
   "name": "Emp Name",
   "phone": "5555555555",
   "email": "email@address.com",
   "department": "Joker",
   "manager": "Some clueless Pleb",
-  "emailgroups": [
+  "email_groups": [
     "test",
     "foo"
     ],
-  "assets": {
+  "assetlist": {
       "Laptop": {
         "asset_tag": "1234",
-        "encrypted": "1" 
-    }
+        "encrypted": 1
+    },
+      "Tablet": {
+        "asset_tag": "4422",
+        "encrypted": 0
+     }
+
   }
 }`
 
 emp := Employee{}
-err = json.Unmarshal([]byte(df), &emp)
+json.Unmarshal([]byte(df), &emp)
 fmt.Println(&emp)
-c.Insert(emp)
+err := c.Insert(emp)
+if err != nil {
+  log.Fatal(err.Error())
+}
 
+/*
+err := c.Find(bson.M{"name": "Emp Name"}).One(&emp)
 
-err = c.Find(bson.M{"name": "Emp Name"}).One(&emp)
-err = nil
 if err != nil {
   fmt.Println("Insert")
   log.Fatal(err)
   }
+*/
 
 fmt.Println("Phone:" , emp.Phone)
 
